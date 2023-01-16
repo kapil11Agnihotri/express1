8c86a4b9d9b11788e9b24bb608316c23879ff314
kapil11Agnihotri <kapil90444@gmail.com>
2023-01-16 12:46:43 +0530

    pages added

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const http=require('http')
 
 const server=http.createServer((req,res)=>{
-    console.log('kapil')
+   const url=req.url;
+   if(url==='/'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page </title></head>');
+    res.write('<body><h1>Welcome to my node js server!</h1></body>');
+    res.write('</html>');
+    res.end()
+   }
+   if(url==='/home'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page </title></head>');
+    res.write('<body><h1>Welcome home</h1></body>');
+    res.write('</html>');
+    res.end(); 
+   }
+   if(url==='/about'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page </title></head>');
+    res.write('<body><h1>Welcome to About Us page</h1></body>');
+    res.write('</html>');
+    res.end(); 
+   }
+   if(url==='/node'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page </title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    res.end(); 
+   }
+    
 })
 
 server.listen(3000)
