34cf72e67e78b31dc09e73be2a9b0bcf4080a437
kapil11Agnihotri <kapil90444@gmail.com>
2023-01-19 14:15:11 +0530

    body parser implemented

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const bodyParser=require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In a Middleware");
-  next();
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use("/add-products", (req, res, next) => {
+  res.send(
+    '<form action="/products" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
+  );
 });
-app.use((req, res, next) => {
-  console.log("In a second Middleware");
+
+app.post("/products", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
 
