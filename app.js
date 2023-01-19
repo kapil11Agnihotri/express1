f8a7f51431656c50dac267d9661e63d5595c2e8a
kapil11Agnihotri <kapil90444@gmail.com>
2023-01-19 14:51:51 +0530

    routes added to app

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const express = require("express");
-const bodyParser=require('body-parser')
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}))
+const adminRoutes = require("./routes/Admin");
+const shopRoutes = require("./routes/Shop");
 
-app.use("/add-products", (req, res, next) => {
-  res.send(
-    '<form action="/products" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/products", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use((req,res,next)=>{
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000);
